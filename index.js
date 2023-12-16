578aa55a40f90e62732f1d9043ce1e0954fe327e
Miguel Leonar <migsldev@gmail.com>
2023-12-16 23:27:16 +1100

    added conditionals on functions

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceFeet){
   // Write your code here!
-}
+  if (distanceFeet <= 400) {
+    return 'This one is on me!'
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  } else if (distanceFeet > 400 && distanceFeet < 2000) {
+    return "That will be twenty bucks."
+    
+  } else if (distanceFeet > 2000 && distanceFeet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  
+  } else if (distanceFeet > 2500) {
+    return "No can do."
+  
+  }
+} 
+console.log(scuberGreetingForFeet(199)); // 199 feet
+console.log(scuberGreetingForFeet(1500)); // 1500 feet
+console.log(scuberGreetingForFeet(2001)); // 2001 feet
+console.log(scuberGreetingForFeet(2501)); // 2501 feet
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === "NYC" ? 'Ok, sounds good.': 'No go.';
 }
+console.log(ternaryCheckCity("NYC"));
+console.log(ternaryCheckCity("Pittsburgh"));
+
+
+function switchOnCharmFromTip(tipValue){
+  // Write your code here!
+  switch(tipValue) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
+}
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
